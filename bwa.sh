9db392df016a3bcdd4f16fc4bf2e718eda57f382
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-01 13:58:54 -0700

    Update bwa.sh

--- bwa.sh
+++ bwa.sh
@@ -9,8 +9,8 @@
 
 IFS=$'\t'
 
-fastqdir=/carc/scratch/projects/ddomman2016191/Picante/fastqs
-ref=/carc/scratch/projects/ddomman2016191/Picante/reference_genome/Sin_Nombre_NMR11_merged.fasta
+fastqdir=Picante/fastqs
+ref=Picante/reference_genome/Sin_Nombre_NMR11_merged.fasta
 
 # Merge Sin Nombre Reference NMR11 (L37902-L37904)
 cat *.fasta > Sin_Nombre_NMR11_merged.fasta
@@ -22,4 +22,4 @@ cat *.fasta > Sin_Nombre_NMR11_merged.fasta
 while read R1 R2; do
 	base=$(echo ${R1} | cut -f 1 -d _ )
 	/users/psh102/repo/bwa/bwa mem ${ref} ${fastqdir}/${R1} ${fastqdir}/${R2} -t 4 > ${base}.sam
-done < /carc/scratch/projects/ddomman2016191/Picante/reads.txt
+done < Picante/reads.txt
