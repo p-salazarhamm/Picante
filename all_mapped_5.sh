0082086fb3edfcafd2169a9fd4fb33e34a8b5669
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-02 20:45:30 -0700

    Update and rename depth.sh to all_mapped_5.sh

---
+++ all_mapped_5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=conda\o
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBARCH --cpus-per-task=16
+#SBATCH --time=48:00:00
+#SBATCH --job-name=all_mapped
+
+module load samtools
+
+array=(TR115-Lung.sam \
+	TR79-Heart.sam \
+	TR79-Liver.sam \
+	TR71-Lung.sam \
+	TR71-Heart.sam \
+	XP13-Lung.sam )
+
+#All reads mapped
+
+for f in ${array[@]}; do
+	base=$(echo $f | sed 's/.sam//')
+samtools view -F 4 -h ${f} > ${base}_mapped.bam
+done
--- depth.sh
+++
@@ -1,3 +0,0 @@
-
-#all reads mapped
-samtools view -F 4 -h ${f} > ${base}_mapped.bam
