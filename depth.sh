f31f132def322f9d3229b3acb4682477579255f4
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-02 20:47:45 -0700

    Create depth.sh

---
+++ depth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=condo
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBARCH --cpus-per-task=16
+#SBATCH --time=48:00:00
+#SBATCH --job-name=depth
+
+module load samtools
+
+array=(TR115-Lung_mapped.sam \
+	TR79-Heart_mapped.sam \
+	TR79-Liver_mapped.sam \
+	TR71-Lung_mapped.sam \
+	TR71-Heart_mapped.sam \
+	XP13-Lung_mapped.sam)
+
+#All reads mapped
+
+for f in ${array[@]}; do
+	base=$(echo $f | sed 's/.sam//')
+samtools depth -a ${f} -o ${base}_mapped.depth
+done
