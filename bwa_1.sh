5bc2f0c08cf760892d59d26d327db9ee82313291
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-07 12:51:29 -0700

    Update bwa_1.sh
    added Peromyscus maniculatus (host) RNA reference

--- bwa_1.sh
+++ bwa_1.sh
@@ -10,16 +10,17 @@
 IFS=$'\t'
 
 fastqdir=Picante/fastqs
-ref=Picante/reference_genome/Sin_Nombre_NMR11_merged.fasta
+ref1=Picante/reference_genome/Sin_Nombre_NMR11_merged.fasta
+ref2=/Picante/reference_genome/GCF_003704035.1_HU_Pman_2.1.3_rna.fna.gz
 
-# Merge Sin Nombre Reference NMR11 (L37902-L37904)
-cat *.fasta > Sin_Nombre_NMR11_merged.fasta
+#1. Merge Sin Nombre Reference NMR11 (L37902-L37904)
+# cat *.fasta > Sin_Nombre_NMR11_merged.fasta
 
-# Index reference 
-/users/psh102/repo/bwa/bwa index ${ref}
+#2. Index reference 
+# /users/psh102/repo/bwa/bwa index ${ref2}
 
 # Map fastqs to reference
 while read R1 R2; do
 	base=$(echo ${R1} | cut -f 1 -d _ )
-	/users/psh102/repo/bwa/bwa mem ${ref} ${fastqdir}/${R1} ${fastqdir}/${R2} -t 4 > ${base}.sam
+	/users/psh102/repo/bwa/bwa mem ${ref2} ${fastqdir}/${R1} ${fastqdir}/${R2} -t 4 > ${base}.sam
 done < Picante/reads.txt
