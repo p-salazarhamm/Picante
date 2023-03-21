a815fe8a6e32d20533bddde96f1cb45a796fca34
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-21 11:51:36 -0600

    Create run_nf_viralrecon_SARS-Cov2.sh

---
+++ run_nf_viralrecon_SARS-Cov2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=condo
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH --job-name=nf_viralrecon
+
+module load singularity
+
+nextflow run nf-core/viralrecon \
+	--input reads.csv \
+	-c nextflow.config \
+	--outdir SARS-Cov2 \
+	--platform illumina \
+	--protocol metagenomic \
+	--genome 'MN908947.3' \
+	-profile singularity \
+	--kraken2_db '/carc/scratch/projects/ddomman2016191/kraken2_databases/k2_pluspf_20220908.tar.gz'
