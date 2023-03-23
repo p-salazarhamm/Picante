ca746f191a105f24dbc487611b5c005257ab5a1a
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-23 10:44:46 -0600

    Update run_nf_viralrecon_SARS-Cov2.sh
    add resume to cache previously completed tasks

--- run_nf_viralrecon_SARS-Cov2.sh
+++ run_nf_viralrecon_SARS-Cov2.sh
@@ -7,12 +7,14 @@
 #SBATCH --job-name=nf_viralrecon
 
 module load singularity
+#use nextflow_hopper.config
 
 nextflow run nf-core/viralrecon \
 	--input reads.csv \
-	-c nextflow_hopper.config \
+	-c nextflow.config \
 	--outdir SARS-Cov2 \
 	--platform illumina \
 	--protocol metagenomic \
 	--genome 'MN908947.3' \
 	-profile singularity
+	-resume
