aaaf43144e80f3b8ebdbf74f19b8ef40dc591c7d
Paris Salazar-Hamm <103594722+p-salazarhamm@users.noreply.github.com>
2023-03-23 11:58:16 -0600

    Create run_nf_viralrecon_hanta.sh

---
+++ run_nf_viralrecon_hanta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --partition=general
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=48:00:00
+#SBATCH --job-name=viralrecon_hanta
+
+module load singularity
+
+nextflow run nf-core/viralrecon \
+	--input reads.csv \
+	-c nextflow.config \
+	--outdir Hanta_viralrecon \
+	--platform illumina \
+	--protocol metagenomic \
+	--fasta reference_genome/SinNombre_NMR11_GCA_002830985.1_genomic.fasta \
+	--gff reference_genome/SinNombre_NMR11_GCA_002830985.1.gff \
+	--skip_pangolin \
+	--skip_nextclade \
+	-profile singularity \
+	-resume
